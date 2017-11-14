48f9d93e98abb070845a732a9614010d80ed2c3e
Kjzth <kundjanasith.t@ku.th>
2017-11-15 04:39:21 +0700

    train error

---
+++ src/regression.R
@@ -0,0 +1,5 @@
+mydata <- read.csv("res_u.csv")
+model <- lm(status ~ cpu_user + cpu_system, data=mydata)
+# print(summary(model))
+# save(model, file="model.data")
+# export.model(model, replace = TRUE)
--- src/regression.py
+++ src/regression.py
@@ -1,7 +1,7 @@
-import statsmodels.api as sm
-from sklearn import datasets
-import numpy as np
-import pandas as pd
+# import statsmodels.api as sm
+# from sklearn import datasets
+# import numpy as np
+# import pandas as pd
 # data = datasets.load_boston()
 # print(data.target)
 
@@ -10,10 +10,10 @@ import pandas as pd
 # target = pd.DataFrame(data.target, columns=['MEDV'])
 # print(df.head())
 # print(target.head())
-df= pd.read_csv("res_u.csv",low_memory=False)
+# df= pd.read_csv("res_u.csv",low_memory=False)
 # print(targetZ.head())
-X = df[['cpu_user','cpu_system']]
-y = df['status']
+# X = df[['cpu_user','cpu_system']]
+# y = df['status']
 
 # X = df[["RM", "LSTAT"]]
 # print(X)
