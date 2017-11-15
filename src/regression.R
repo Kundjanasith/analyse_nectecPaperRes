bf05afdfa1d080e7a5f62391e67b82c50c635922
Ubuntu <hortonworks@Siriventure.n2cicu3ee0aezpiz3tsekm5uoc.ix.internal.cloudapp.net>
2017-11-15 06:25:45 +0000

    res uu

--- src/regression.R
+++ src/regression.R
@@ -1,6 +1,6 @@
-mydata <- read.csv("res_u.csv")
+mydata <- read.csv("res_uu.csv")
 # dat <- dat[!is.na(as.numeric(as.character(dat$A))),]
-model <- lm(status ~ cpu_user + cpu_system, data=mydata)
-# print(summary(model))
-# save(model, file="model.data")
+model <- lm(status ~ cpu_user + cpu_system + mem_trans + mem_act + bytes_in + bytes_out + worker , data=mydata)
+print(summary(model))
+save(model, file="model.data")
 # export.model(model, replace = TRUE)
