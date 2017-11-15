36c4017f49ea521b0efcb061bb29708cccf6cda5
Kjzth <kundjanasith.t@ku.th>
2017-11-15 14:00:33 +0700

    use model

--- src/regression.R
+++ src/regression.R
@@ -2,5 +2,5 @@ mydata <- read.csv("res_uu.csv")
 # dat <- dat[!is.na(as.numeric(as.character(dat$A))),]
 model <- lm(status ~ cpu_user + cpu_system + mem_trans + mem_act + bytes_in + bytes_out + worker , data=mydata)
 print(summary(model))
-save(model, file="model.data")
+saveRDS(model, file="model.data")
 # export.model(model, replace = TRUE)
