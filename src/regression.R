d2e1933ea4e08eeb78f325e15286902900af82e6
Kjzth <kundjanasith.t@ku.th>
2017-11-15 09:51:41 +0700

    python regres

--- src/regression.R
+++ src/regression.R
@@ -1,4 +1,5 @@
 mydata <- read.csv("res_u.csv")
+# dat <- dat[!is.na(as.numeric(as.character(dat$A))),]
 model <- lm(status ~ cpu_user + cpu_system, data=mydata)
 # print(summary(model))
 # save(model, file="model.data")
--- src/regression.py
+++ src/regression.py
@@ -26,3 +26,18 @@
 # Z = pd.DataFrame(z)
 # #Z = Z[["RM", "LSTAT"]]
 # print(model.predict(Z))
+
+from sklearn.linear_model import LinearRegression
+import pandas as pd
+lr = LinearRegression()
+data = pd.read_csv("res_u.csv",low_memory=False)
+print(data.head())
+del data['time']
+print(data.head())
+
+mydata = data[data['cpu_user']!="cpu_user"]
+
+import statsmodels.formula.api as sm
+model = sm.ols(formula='status~cpu_user+cpu_system,mem_trans,mem_act,size,bytes_in,bytes_out,worker', data=mydata)
+fitted = model.fit()
+print(fitted.summary())
